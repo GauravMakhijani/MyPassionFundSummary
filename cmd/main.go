4b453dce8573127257afbe3b7493d8e16ef8f44c
Gaurav Makhijani <makhijani.gaurav19@gmail.com>
2023-04-04 13:08:44 +0530

    intialize poc

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/GauravMakhijani/MyPassionFundSummary/internal/controller"
+	"github.com/GauravMakhijani/MyPassionFundSummary/server"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/negroni"
+)
+
+func main(){
+    logrus.SetFormatter(&logrus.TextFormatter{
+        FullTimestamp: true,
+        TimestampFormat: "2006-01-02 15:04:05",
+    })
+
+    deps,err := server.InitDependencies()
+    if err != nil {
+        logrus.WithError(err).Error("Failed to initialize dependencies")
+        return
+    }
+    router := controller.InitRouter(deps)
+    server := negroni.Classic()
+    server.UseHandler(router)
+    server.Run(":8080")
+}
